721d06d1d59b6ccc53d3bb88bc44cc0067c0ec97
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2024-05-07 20:50:58 -0300

    Criacao do logout e controle de acesso.

--- js/script-sucesso.js
+++ js/script-sucesso.js
@@ -2,6 +2,17 @@
 
 //Recuperando o objeto do usuario logado.
  const usuario = JSON.parse(localStorage.getItem("usuario-logado"));
- 
+
+ if(usuario){ 
  const elViewUser = document.querySelector("#view-user");
  elViewUser.innerText = usuario.emailUsuario;
+
+ const elLogoutUser = document.querySelector("#logout-user");
+ elLogoutUser.addEventListener("click",()=>{
+    localStorage.removeItem("usuario-logado");
+    window.location.href = "../index.html";
+ });
+}
+else{
+    window.location.href = "../index.html";
+}
