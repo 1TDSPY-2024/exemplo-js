8bdb0551ca7daaa001fd5917f1eca6daf7642ec5
victorcosmedev <victorcosme.profissional@gmail.com>
2024-05-07 20:52:16 -0300

    Criação do logout e controle de acesso.

--- js/script-sucesso.js
+++ js/script-sucesso.js
@@ -1,6 +1,17 @@
 //Recuperando o objeto do usuario logado.
 const usuario = JSON.parse(localStorage.getItem("usuario-logado"));
- 
-const elViewUser = document.querySelector("#view-user");
 
-elViewUser.innerText = usuario.emailUsuario;
+if(usuario){
+    const elViewUser = document.querySelector("#view-user");
+
+    elViewUser.innerText = usuario.emailUsuario;
+
+    const elLogoutUser = document.querySelector("#logout-user");
+    elLogoutUser.addEventListener("click",()=>{
+        localStorage.removeItem("usuario-logado");
+        window.location.href = "../index.html";
+    });
+} 
+else {
+    window.location.href = "../index.html";
+}
