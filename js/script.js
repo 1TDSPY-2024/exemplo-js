05af34eff7e0d1ec308f35cce573370eaef6859b
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2024-05-10 20:24:39 -0300

    Visualização de senhas com icones e funções.

--- js/script.js
+++ js/script.js
@@ -291,11 +291,28 @@ let listaUsuarios = [
     {emailUsuario:"manoel@email.com",senhaUsuario:"123456"},
 ];
 
-// listaUsuarios.forEach(function(usuario){
-//     if(usuario.emailUsuario == "joaquim@email.com"){
-//         console.log(usuario.emailUsuario);
-//     }
-// });
+const alteraVisao = (inputSenha)=>{
+    if(inputSenha.type == "password"){
+        inputSenha.setAttribute("type","text");
+    }else{
+        inputSenha.setAttribute("type","password");
+    }
+}
+
+
+let iconEye = document.querySelector("i");
+iconEye.addEventListener('click', function(){
+    
+    let inputSenha = document.querySelector("#idSenha");
+
+    if(this.className == "fa-regular fa-eye-slash" ){
+        this.setAttribute("class","fa-regular fa-eye");
+        alteraVisao(inputSenha);
+    }else{
+        this.setAttribute("class","fa-regular fa-eye-slash");
+        alteraVisao(inputSenha);
+    }
+});
 
 function validaCampos(input1,input2) {
    
