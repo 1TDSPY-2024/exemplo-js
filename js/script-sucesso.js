663a52f80da77c5a9fb9e5c87d79b78746478d79
Victor Cosme <120526474+victorcosmedev@users.noreply.github.com>
2024-05-10 20:56:56 -0300

    Sistema de autenticação com tokenUser e Math.

--- js/script-sucesso.js
+++ js/script-sucesso.js
@@ -1,13 +1,17 @@
-//Recuperando o objeto do usuario logado.
-const usuario = JSON.parse(localStorage.getItem("usuario-logado"));
+// Reuperando o token através do sessionStorage
+let tokenUser = sessionStorage.getItem("token");
 
-if(usuario){
+if(tokenUser != "" && tokenUser != undefined){
+
+    //Recuperando o objeto do usuario logado.
+    const usuario = JSON.parse(localStorage.getItem("usuario-logado"));
     const elViewUser = document.querySelector("#view-user");
 
     elViewUser.innerText = usuario.emailUsuario;
 
     const elLogoutUser = document.querySelector("#logout-user");
     elLogoutUser.addEventListener("click",()=>{
+        sessionStorage.removeItem("token");
         localStorage.removeItem("usuario-logado");
         window.location.href = "../index.html";
     });
--- js/script.js
+++ js/script.js
@@ -336,6 +336,10 @@ function validaCampos(input1,input2){
             // localStorage.setItem("chave-chave", "valor");
             localStorage.setItem("usuario-logado", JSON.stringify(listaUsuarios[x]));
 
+            // Criando o token de autenticação do usuário
+            const tokenUser = Math.random().toString(16).substring(2) + Math.random().toString(16).substring(2);
+            // Criando o objeto na sessionStorage e guardando o token;
+            sessionStorage.setItem("token", tokenUser);
 
             //Atribuir uma nova classe ao elemento de msg do usuario.
             elMsg.setAttribute("class","sucesso");
