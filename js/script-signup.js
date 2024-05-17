466032cd25ad16d5d71a8671f7094a6e70557ca7
Vinicius Leandro <111243990+ViniciusLABernardes@users.noreply.github.com>
2024-05-17 19:28:56 -0300

    Add files via upload

--- js/script-signup.js
+++ js/script-signup.js
@@ -1,60 +1,23 @@
-
-let senha = document.getElementById("idSenha");
-let senhaConf = document.getElementById("idConfSenha");
-let iconEye = document.querySelector("i");
-let iconEye2 = document.querySelector("i2");
-iconEye.addEventListener('click', function(){
-    if(this.className == "fa-regular fa-eye-slash"){
-        this.setAttribute("class","fa-regular fa-eye");
-        senha.setAttribute("type", "text");
-        
-    }else{
-        this.setAttribute("class","fa-regular fa-eye-slash");
-        senha.setAttribute("type", "password");
-        
-    }
-})
-iconEye2.addEventListener('click', function(){
-    if(this.className == "fa-regular fa-eye-slash"){
-        this.setAttribute("class","fa-regular fa-eye");
-        senhaConf.setAttribute("type", "text");  
-    }else{
-        this.setAttribute("class","fa-regular fa-eye-slash");
-        senhaConf.setAttribute("type", "password");
-    }
-})
-
-const registraUsuario = (inputNome, inputCpf, 
-    inputGen, inputEmail, inputSenha, inputConfSenha) =>{
-
-        if(inputSenha.value == inputConfSenha.value){
-            //deixando o label verde pq as senhas conferem
-            document.querySelector("label[for=idConfirmarSenha").setAttribute("class","sucesso");
-            //voltando o campo ao normal quando recebe o foco;
-            inputConfSenha.addEventListener("focus",()=>{
-                document.querySelector("label[for=idConfirmarSenha]").setAttribute("class","");
-            });
-
-            const usuario = {
-                nome: inputNome,
-                cpf: inputCpf.value,
-                genero: inputGen.value,
-                email: inputEmail.value,
-                senha: inputSenha.value
-            }
-
-            //recupera o banco do localStorage e adiciona o objeto usuario na listaUser
-            let listaUser = JSON.parse(localStorage.getItem("listaUser"));
-
-            //adicionando de fato o objeto na lista com o metodo push
-            listaUser.push(usuario);
-
-            localStorage.setItem("listaUser",JSON.stringify(listaUser));
-
-            return false;
-        }else{
-            //deixando o label em vermelho pq as senhas n coincidem
-            document.querySelector("label[for=idConfirmarSenha]").setAttribute("class","erro");
-            return false;
+const registraUsuario = (inputNome, inputCpf, inputGen, inputEmail, inputSenha, inputConfirmaSenha) => {
+    
+    if(inputSenha.value == inputConfirmaSenha.value) {
+        const usuario = {
+            txtNome: inputNome.value,
+            txtCpf : inputCpf.value,
+            gen : inputGen.value,
+            txtEmail : inputEmail.value,
+            txtSenha : inputSenha.value
         }
+
+        let listaUsuarios = JSON.parse(localStorage.getItem("base-dados")) || [];
+
+        listaUsuarios.push(usuario);
+
+        localStorage.setItem("base-dados", JSON.stringify(listaUsuarios));
+
+    } else {
+        // deixando o label vermelho pq as senhas nao estao iguais
+        document.querySelector("label[for=idConfirmaSenha]").setAttribute("class", "erro");
+        return false;
+    }
 }
--- js/script-sucesso.js
+++ js/script-sucesso.js
@@ -1,19 +1,20 @@
+let tokenUser = sessionStorage.getItem("token")
+if(tokenUser != "" && tokenUser != undefined) {
+    //Recuperando o objeto do usuario logado.
+    const usuario = JSON.parse(localStorage.getItem("usuario-logado"));
 
+    if(usuario){ 
+    const elViewUser = document.querySelector("#view-user");
+    elViewUser.innerText = usuario.txtNome;
 
-//Recuperando o objeto do usuario logado.
- const usuario = JSON.parse(localStorage.getItem("usuario-logado"));
-
- if(usuario){ 
- const elViewUser = document.querySelector("#view-user");
- elViewUser.innerText = usuario.txtEmail;
-
- const elLogoutUser = document.querySelector("#logout-user");
- elLogoutUser.addEventListener("click",()=>{
-    sessionStorage.removeItem("token");
-    localStorage.removeItem("usuario-logado");
-    window.location.href = "../index.html";
- });
-}
-else{
-    window.location.href = "../erro.html";
+    const elLogoutUser = document.querySelector("#logout-user");
+    elLogoutUser.addEventListener("click",()=>{
+        sessionStorage.removeItem("token")
+        localStorage.removeItem("usuario-logado");
+        window.location.href = "../index.html";
+    });
+    }
+    else{
+        window.location.href = "../status/erro.html";
+    }
 }
